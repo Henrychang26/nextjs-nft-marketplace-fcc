ec3aa667978d9782931f01577113311792f2057f
Henrychang26 <henrychang26@gmail.com>
2022-09-09 15:08:58 -0700

    front-end scripts and event file

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from "next/head"
 // import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Hi!
-        </div>
-    )
+    //how do we show recently listed NFT
+
+    //we will read from a database that has all the mappings in an easier to read data structure
+    //we will index the events off-chain and the read from our database
+    //setup a server to listen for events to be fired, and we will add them to a database query.
+
+    //TheGraph does this in decentralized way
+    //Moralis does it in a centralized way
+    return <div className={styles.container}>Hi!</div>
 }
